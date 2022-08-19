ef22ac1c8e011d0401739174c583765f3479022a
William Tinta <williammatheustnt@gmail.com>
2022-08-19 15:32:50 -0300

    Atualizacao 2 de 19/08

--- aula1Professor/src/TesteProdutoCategoria.java
+++ aula1Professor/src/TesteProdutoCategoria.java
@@ -18,9 +18,16 @@ import javax.persistence.TypedQuery;
  * @author aluno
  */
 public class TesteProdutoCategoria {
+        private static ProdutoDAO produtaDAO = new ProdutoDAO();
+        private static CategoriaDAO categoriaDAO = new CategoriaDAO();
 
     public static void main(String[] args) {
-        
+        adicionarProdutos();
+        reajustarPreco(new BigDecimal(5.0));
+
+    }
+    
+    private static void teste(){
         Categoria categoria = new Categoria();
         categoria.setDescricao("Informatica");
         
@@ -48,6 +55,27 @@ public class TesteProdutoCategoria {
         for (Produto p : produtoDAO.buscar()) {
             System.out.println("Descricao: " + p.getDescricao());
         }
+    }
+    
+    private static void adicionarProdutos(){        
+        
+        for(int i = 0; i < 10; i++){
+            Categoria categoria = new Categoria();
+            categoria.setDescricao("Categoria " + i);
+            categoriaDAO.inserir(categoria);
+            
+            Produto produto = new Produto();
+            produto.setDescricao("Produto " + i);
+            produto.setPreco(new BigDecimal(60.0 * (i+1)));
+            produto.setQuantidade(100);
 
+            produto.setCategoria(categoria);
+            
+            produtaDAO.inserir(produto);
+        }
+    }
+    
+    private static void reajustarPreco(BigDecimal percentual){
+        System.out.println(produtaDAO.buscar());
     }
 }
